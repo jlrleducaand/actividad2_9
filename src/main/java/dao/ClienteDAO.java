0ea80055a7c4e3139ba2138d77bd2015e46f11fb
jlrl <goldteamspain@gmail.com>
2023-12-21 13:24:09 +0100

    error depurado en git y  grabar.jsp

--- src/main/java/dao/ClienteDAO.java
+++ src/main/java/dao/ClienteDAO.java
@@ -1,7 +1,6 @@
 package dao;
 
 import model.Cliente;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -10,8 +9,11 @@ public interface ClienteDAO {
     public void create(Cliente cliente);
 
     public List<Cliente> getAll();
+
     public List<Cliente> find(String nombre);
+
     public Optional<Cliente> find(int id);
+
     public void update(Cliente cliente);
 
     public void delete(int id);
